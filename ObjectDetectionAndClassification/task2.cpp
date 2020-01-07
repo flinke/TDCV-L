034d6c3700f6b3ab4c9c9998d9cdcbeba0ffd2bc
mokrueger <59337243+mokrueger@users.noreply.github.com>
2020-01-07 04:48:52 +0100

    Update task2.cpp

--- ObjectDetectionAndClassification/task2.cpp
+++ ObjectDetectionAndClassification/task2.cpp
@@ -8,34 +8,100 @@
 #include "HOGDescriptor.h"
 #include "RandomForest.h"
 #include "task1.h"
-#include "task2.h"
 
 
 using namespace std;
 using namespace cv;
 namespace fs = std::filesystem;
 
+vector<string> list_dir(string path);
+Mat getRandomSample(Mat& entireData, int sampleSize);
+void mergeVectorToSingleMats(Mat& train, Mat& test, vector<Mat>& train_data, vector<Mat>& test_data);
+vector<list<vector<float>>> getAllDescriptors(string filepath, bool getRotatedSamples = true);
+vector<Mat> convertToMatVector(vector<list<vector<float>>>& allDescriptors);
+vector<vector<string>> getAllClassPaths(string filepath);
+RandomForest forest;
+
 
 template<class ClassifierType>
-void performanceEval(cv::Ptr<ClassifierType> classifier, cv::Ptr<cv::ml::TrainData> test_data) {
+void performanceEval(cv::Ptr<ClassifierType> treeOrForest, Mat data) {
 
-	/*
-		TODO
-	*/
+	// Masks 
+	Rect rCrop = Rect(0, 0, data.cols - 1, data.rows);
+	Rect rvCrop = Rect(data.cols - 1, 0, 1, data.rows);
 
+	// Get response Vector
+	Mat testResponseVector;
+	data(rvCrop).convertTo(testResponseVector, CV_32S);
+
+	Mat predictOutput;
+	treeOrForest->predict(data(rCrop), predictOutput, cv::ml::DTrees::PREDICT_MAX_VOTE);
+
+	// Output to console
+	//cout << "predictOutput = " << endl << " " << predictOutput.t() << endl << endl; //Output transposed
+	//cout << "groundTruth = " << endl << " " << testResponseVector.t() << endl << endl; //Output transposed
+
+	// Calc Misses vs Ground truth
+	int hits = 0;
+	int misses = 0;
+	for (int i = 0; i < testResponseVector.rows; i++) {
+		if (predictOutput.at<int>(i, 0) == testResponseVector.at<int>(i, 0)) {
+			hits++;
+		}
+		else {
+			misses++;
+		}
+	}
+	cout << "Hits: " << hits << "; Misses: " << misses << "; Accuracy: " << (float) hits/(misses+hits) << endl << endl;
 
 }
 
 
 void testDTrees(vector<Mat>  train_data, vector<Mat> test_data) {
 
-    int num_classes = train_data.size(); // Is this equal to the size of train_data / test_data? else set it back to 6
-
+    //int num_classes = 6;
 	Ptr<cv::ml::DTrees> tree = cv::ml::DTrees::create();
 	tree->setCVFolds(1); //Geht nicht mit anderen Zahlen größer als 1 (warum?)
 	tree->setMaxCategories(10); //Standard 10;
 	tree->setMaxDepth(INT8_MAX);
 	tree->setMinSampleCount(2); //Standard 10; Weniger = besser (zumindest hier?)
+
+	// Fügt alle Mats aus dem vector<Mat> zusammen (unschön geschrieben)
+	Mat train;
+	Mat test;
+	mergeVectorToSingleMats(train, test, train_data, test_data); //quick and dirty
+
+	// Maks
+	Rect rCropForTrain = Rect(0, 0, train.cols - 1, train.rows); //Mask to get only the data
+	Rect rvCropForTrain = Rect(train.cols - 1, 0, 1, train.rows); //Mask to get only the classLables
+
+	// Extract responseVector and convert to CV_32S (else will not be recognized as a lable)
+	Mat trainResponseVector;
+	train(rvCropForTrain).convertTo(trainResponseVector, CV_32S);
+
+	// Train with data + reponseVector
+	tree->train(train(rCropForTrain), cv::ml::ROW_SAMPLE, trainResponseVector);
+
+    performanceEval<cv::ml::DTrees>(tree, train);
+    performanceEval<cv::ml::DTrees>(tree, test);
+}
+
+
+void testForest(vector<Mat> training_data, vector<Mat> test_data){
+	
+    //int num_classes = 6;
+	int treeCount = 200;
+	int maxDepth = 10;
+	int cvFolds = 1;
+	int minSampleCount = 1;
+	int maxCategories = 11;
+
+	forest = RandomForest(treeCount, maxDepth, cvFolds, minSampleCount, maxCategories); //treecount, maxdepth (default: intmax), cvfolds (default: 10), minsamplecount (default: 10), maxcategories (default: 10)
+	Ptr<RandomForest> forestPtr = &forest;
+	// Fügt alle Mats aus dem vector<Mat> zusammen (unschön geschrieben)
+	Mat train;
+	Mat test;
+	mergeVectorToSingleMats(train, test, training_data, test_data); //quick and dirty
 
 	// Mask
 	Rect rCropForTrain = Rect(0, 0, train.cols - 1, train.rows); //Mask to get only the data
@@ -45,75 +111,47 @@ void testDTrees(vector<Mat>  train_data, vector<Mat> test_data) {
 	Mat trainResponseVector;
 	train(rvCropForTrain).convertTo(trainResponseVector, CV_32S);
 
-	// Train with data + reponseVector
-	cout << "training our tree ..." << endl;
-	tree->train(train(rCropForTrain), cv::ml::ROW_SAMPLE, trainResponseVector);
-
-	// Masks
-	Rect rCropForTest = Rect(0, 0, test.cols - 1, test.rows);
-	Rect rvCropForTest = Rect(test.cols - 1, 0, 1, test.rows); 
-
-	// Convert to CV_32S (previously class defined as float)
-	Mat testResponseVector;
-	test(rvCropForTest).convertTo(testResponseVector, CV_32S);
-
 	Mat predictOutput;
-	tree->predict(test(rCropForTest), predictOutput, cv::ml::DTrees::PREDICT_MAX_VOTE);
-	cout << "predictOutput = " << endl << " " << predictOutput.t() << endl << endl; //Output transposed
-	cout << "groundTruth = " << endl << " " << testResponseVector.t() << endl << endl; //Output transposed
+	forestPtr->train(train);
 
-	// Calc Misses vs Ground truth
-	int hits = 0;
-	int misses = 0;
-	for (int i = 0; i < testResponseVector.rows; i++) {
-		if (predictOutput.at<int>(i, 0) == testResponseVector.at<int>(i,0)) {
-			hits++;
-		}
-		else {
-			misses++;
-		}
-	}
-	cout << "Hits: " << hits << "; Misses: " << misses << endl << endl;
-	cout << "Using: Predict_max_vote" << endl << endl;
-
-    //performanceEval<cv::ml::DTrees>(tree, train_data);
-    //performanceEval<cv::ml::DTrees>(tree, test_data);
-
-}
-
-
-void testForest(vector<Mat> training_data, vector<Mat> test_data){
-	
-    int num_classes = 6;
-
-    /* 
-      * 
-      * Create your data (i.e Use HOG from task 1 to compute the descriptor for your images)
-      * Train a Forest and evaluate the performance 
-      * Experiment with the MaxDepth & Count parameters, to see how it affects the performance
-
-    */
-
-    //performanceEval<RandomForest>(forest, train_data);
-    //performanceEval<RandomForest>(forest, test_data);
+	//performanceEval<RandomForest>(forestPtr, train);
+    performanceEval<RandomForest>(forestPtr, test);
 }
 
 
 int main(){
 
-	forest = RandomForest(100, 10, 1, 50, 10); //treecount, maxdepth (default: intmax), cvfolds (default: 10), minsamplecount (default: 10), maxcategories (default: 10)
-
 	//single descriptors can be accessed via vector[class(0-5)][image*8 or image] depending on if rotatedSamples = true/false
 	//TODO Convert into training data
-	cout << "computing descriptors ..." << endl;
 	vector<list<vector<float>>> trainTemp = getAllDescriptors("data/task2/train/");
 	vector<list<vector<float>>> testTemp = getAllDescriptors("data/task2/test/", false);
 	vector<Mat> allTrainingDescriptors = convertToMatVector(trainTemp);
 	vector<Mat> allTestingDescriptors = convertToMatVector(testTemp);
-	testDTrees(allTrainingDescriptors, allTestingDescriptors);
-    //testForest();
+	Mat train;
+	Mat test;
+	//mergeVectorToSingleMats(train, test, allTrainingDescriptors, allTestingDescriptors);
+	//testDTrees(allTrainingDescriptors, allTestingDescriptors);
+    testForest(allTrainingDescriptors, allTestingDescriptors);
     return 0;
 }
+
+Mat getRandomSample(Mat& entireData, int sampleSize) {
+	return entireData;
+}
+
+void mergeVectorToSingleMats(Mat& train, Mat& test, vector<Mat>& train_data, vector<Mat>& test_data) { 
+	// SEHR SEHR SEHR UNSCHÖN UND FUNKTIONIERT NUR WENN KLASSENZAHL BLEIBT; BITTE FIXEN
+	int num_classes = 6;
+	Mat train = train_data[0];
+	Mat test = test_data[0];
+	for (int i = 1; i < num_classes; i++)
+	{
+		vconcat(train, train_data[i], train);
+		vconcat(test, test_data[i], test);
+	}
+
+}
+
 
 
 //function that gets all descriptors for all (output = [class[0], class[1], ...] with class[0] = [hogforimg1, hogforimg2, ...]
@@ -139,15 +177,13 @@ vector<Mat> convertToMatVector(vector<list<vector<float>>>& allDescriptors) {
 
 	vector<Mat> descriptorsAsMatVector;
 
-	for (float classNumber = 0; classNumber < allDescriptors.size(); classNumber++)
-	{
-		list<vector<float>> singleClass = allDescriptors[classNumber];
+	float classNumber = 0;
+	for (list<vector<float>> singleClass : allDescriptors) {
 		int rows = singleClass.size();
 		int cols = singleClass.front().capacity();
 		Mat tempMat = Mat(rows, cols + 1, CV_32FC1);
 		int j = 0;
-		for (vector<float> singleDescriptor : singleClass) 
-		{
+		for (vector<float> singleDescriptor : singleClass) {
 			for (int i = 0; i < cols; i++) {
 				tempMat.at<float>(j, i) = singleDescriptor[i];
 			}
@@ -156,8 +192,8 @@ vector<Mat> convertToMatVector(vector<list<vector<float>>>& allDescriptors) {
 		}		
 		//cout << "M = " << endl << " " << tempMat << endl << endl;
 		descriptorsAsMatVector.push_back(tempMat);
+		classNumber++;
 	}
-	
 	return descriptorsAsMatVector;
 }
 
