b4ccd17f2c9c8859d77ddd674bc82fbb891d9809
mokrueger <moritz.krueger@fn.de>
2020-01-08 18:19:05 +0100

    Updated

--- ObjectDetectionAndClassification/task3.cpp
+++ ObjectDetectionAndClassification/task3.cpp
@@ -6,8 +6,6 @@
 
 #include "HOGDescriptor.h"
 #include "RandomForest.h"
-#include "task1.h"
-#include "task2.h"
 #include "task3.h"
 
 
@@ -21,4 +19,29 @@ void drawBox(Mat img, Rect window, int classID, float confidence)
 	{
 
 	}
+}
+
+int main() {
+
+	// Setup Forest
+	int treeCount = 350;
+	int maxDepth = 15;
+	int cvFolds = 1;
+	int minSampleCount = 1;
+	int maxCategories = 15;
+	forest = RandomForest(treeCount, maxDepth, cvFolds, minSampleCount, maxCategories);
+
+	// Setup Training Data
+	vector<list<vector<float>>> trainTemp = getAllDescriptors("data/task3/train/");
+	vector<Mat> allTrainingDescriptors = convertToMatVector(trainTemp);
+	Mat train;
+
+	// Train Forest
+	cout << "training our forest ..." << endl;
+	forest.train(allTrainingDescriptors, 100);
+
+
+
+	cout << "DONE" << endl;
+	return 0;
 }
